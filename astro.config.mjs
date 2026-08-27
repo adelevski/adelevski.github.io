4397c970dc2ac2c30f12a2eb8f40303608454852
Atanas Delevski <31967176+adelevski@users.noreply.github.com>
2026-08-26 23:56:56 -0500

    feat: publish choosing-a-center article

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
+import { unified } from "@astrojs/markdown-remark";
 import { defineConfig } from "astro/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 export default defineConfig({
   site: "https://adelevski.github.io",
   output: "static",
+  markdown: {
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+  },
 });
